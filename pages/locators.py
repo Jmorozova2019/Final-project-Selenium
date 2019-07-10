ee4bb2bafed1ece933e060d467f7c11318557805
Jmorozova <Jannamoroz10@ya.ru>
2019-07-10 23:29:54 +0300

    Add implementation check cost_basket_equal_cost_product

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,26 @@ class MainPageLocators(object):
 class LoginPageLocators(object):
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+class ProductPageLocators(object):
+    #Название продукта
+    PRODUCT_NAME_ELEM = (By.XPATH, "//div[@class = 'col-sm-6 product_main']/h1")
+
+    #Цена продукта
+    PRODUCT_COST_ELEM = (By.XPATH, "//div[@class = 'col-sm-6 product_main']/p")
+
+    #Кнопка Добавить в корзину
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+
+class BasketPageLocators(object):
+    #Сообщение с названием добавленного товара
+    mess_node_xpath = "//div[@id = 'messages' and @style = 'visibility: visible;']/descendant::strong"
+    MESSAGE_IN_BASKET = (By.XPATH, mess_node_xpath)
+
+    #Элемент Корзина Итого:
+    all_cost_xpath = "//div[@class = 'basket-mini pull-right hidden-xs']"
+    MESSAGE_BASKET_ALL_COST = (By.XPATH, all_cost_xpath)
+    
+    #Сообщение "Стоимость корзины теперь составляет" - часть, относящиеся к цене
+    cost_basket_xpath = "//div[@id = 'messages']/div/div[@class = 'alertinner ']/p/strong"
+    MESSAGE_BASKET_COST = (By.XPATH, all_cost_xpath)
