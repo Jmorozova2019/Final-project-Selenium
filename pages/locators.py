9ac1895081a8b087bccb905173c731ad8410203c
Jmorozova <Jannamoroz10@ya.ru>
2019-07-13 18:14:09 +0300

    Module 4.3 Add BasePageLocators, add test_guest_should_see_login_link_on_product_page and test_guest_can_go_to_login_page_from_product_page

--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,16 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
 
 class LoginPageLocators(object):
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+class BasePageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class ProductPageLocators(object):
     #Название продукта
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
 from pages.base_page import BasePage
+from pages.login_page import LoginPage
 import pytest
 
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -26,7 +28,25 @@ def test_guest_can_add_product_to_cart(browser, link):
     #Добавить в корзину и отправить код
     product_page.click_add_to_basket()
     product_page.solve_quiz_and_get_code()
-    print(link)
+
     #Выполнить проверки
     product_page.should_be_message_about_adding()
     product_page.should_be_cost_basket_equal_cost_product()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    #Проверить, что ссылка на страницу логина существует
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+
+    #Перейти по ссылке на страницу логина
+    page.go_to_login_page()
+    #Проверить, что попали на нужную страницу по адресу
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
